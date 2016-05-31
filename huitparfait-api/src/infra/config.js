7c1fb758115cf651c51137e14dce1003abd03e76
Maxime Ghignet <maxime.ghignet@gmail.com>
2016-06-01 00:25:06 +0200

    chore(api): add ability to require a config.js file for env variables

--- huitparfait-api/src/infra/config.js
+++ huitparfait-api/src/infra/config.js
@@ -3,6 +3,10 @@ import schema from '../../config/default-config'
 
 const conf = convict(schema)
 
+if (process.env.NODE_ENV !== 'production') {
+    conf.loadFile(['config/config.json'])
+}
+
 conf.validate({ strict: true })
 
 export default conf
