9b8915d1837a22f48dd42dcd2ebe218a25f2a860
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-06-08 01:36:51 +0200

    feat(api): filter groups with only isAdmin

--- huitparfait-api/src/routes/users-route.js
+++ huitparfait-api/src/routes/users-route.js
@@ -127,14 +127,13 @@ exports.register = function (server, options, next) {
                 tags: ['api'],
                 handler(req, reply) {
                     cypher(`
-                        MATCH    (:User { id:{id} })-[imog:IS_MEMBER_OF_GROUP]->(g:Group)
+                        MATCH    (:User { id:{id} })-[:IS_MEMBER_OF_GROUP { isAdmin: true, isActive: true }]->(g:Group)
                         MATCH    (u:User)-->(g)
-                        WHERE    imog.isActive = true
                         RETURN   g.name      AS name, 
                                  g.avatarUrl AS avatarUrl, 
                                  g.id        AS id, 
                                  count(u.id) AS userCount
-                        ORDER BY g.name`,
+                        ORDER BY lower(g.name)`,
                         {
                             id: req.auth.credentials.id,
                         })
