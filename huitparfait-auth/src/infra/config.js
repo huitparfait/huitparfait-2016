84beb8fc9489c8e1d0d21f2ad309134880e855dd
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-06-08 22:37:17 +0200

    chore: remove config/config.json (use docker env vars)

--- huitparfait-auth/src/infra/config.js
+++ huitparfait-auth/src/infra/config.js
@@ -3,9 +3,9 @@ import schema from '../../config/default-config'
 
 const conf = convict(schema)
 
-if (process.env.NODE_ENV !== 'production') {
-    conf.loadFile(['config/config.json'])
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     conf.loadFile(['config/config.json'])
+// }
 
 conf.validate({ strict: true })
 
