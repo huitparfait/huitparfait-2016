ae40cc8c7d0077aa5487c64084201174eadd6165
Fabrice CLAEYS <fabrice.claeys@symbol-it.fr>
2016-06-08 20:00:30 +0200

    fix: add loader to file

--- huitparfait-front/build/webpack.base.conf.js
+++ huitparfait-front/build/webpack.base.conf.js
@@ -60,7 +60,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
+        loader: 'file',
         query: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
