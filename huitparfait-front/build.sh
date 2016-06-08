bc36aa43483e02c98ad630afe503f0925453afd1
Fabrice CLAEYS <fabrice.claeys@symbol-it.fr>
2016-06-08 20:01:56 +0200

    chore: the front build script push image to docker hub

--- huitparfait-front/build.sh
+++ huitparfait-front/build.sh
@@ -4,4 +4,6 @@ docker build --rm -f build.Dockerfile -t huitparfait/front-build . && \
 
 docker run -it -v $(pwd)/dist:/app/dist  huitparfait/front-build && \
 
-docker build --rm -f Dockerfile -t huitparfait/huitparfait-front .
+docker build --rm -f Dockerfile -t huitparfait/huitparfait-front . && \
+
+docker push huitparfait/huitparfait-front
