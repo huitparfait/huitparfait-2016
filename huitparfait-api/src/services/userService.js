548c9412e585dc8a966c2bb33692c8824000c970
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-06-19 15:18:37 +0200

    feat: highlight current user in rankings

--- huitparfait-api/src/services/userService.js
+++ huitparfait-api/src/services/userService.js
@@ -11,12 +11,14 @@ export function betterUser(user = {}, transformAnonymous = false) {
 
     if (user.isAnonymous && transformAnonymous) {
         return {
+            id: user.userId,
             name: user.anonymousName,
             avatarUrl: generateSVGDataURIString(user.userId, { width: 70, size: 3 }),
         }
     }
 
     return {
+        id: user.userId,
         name: user.userName,
         avatarUrl: user.avatarUrl,
     }
